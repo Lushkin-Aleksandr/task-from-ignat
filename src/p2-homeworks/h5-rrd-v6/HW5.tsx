b27cee5d1bb950fca301e93c818965767ceb6a93
sanyalu <sanyalu97@gmail.com>
2022-09-15 00:35:34 +0200

    change browser router to hash router for gh-pages

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
-            <BrowserRouter>
+            {/*<BrowserRouter>*/}
                 <Header/>
 
                 <Pages/>
-            </BrowserRouter>
+            {/*</BrowserRouter>*/}
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
