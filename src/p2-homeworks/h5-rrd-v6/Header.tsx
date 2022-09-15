c817b0fe050fecdc74376652d0ed0089ac540fbe
sanyalu <sanyalu97@gmail.com>
2022-09-15 21:14:20 +0200

    create styles for active navlink, change path constants

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,17 +4,29 @@ import {PATH} from "./Pages";
 import styles from './H5.module.css'
 
 function Header() {
+
+
+    const calculateClassForNavLink = ({isActive}: {isActive: boolean}) => {
+        return isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink;
+    };
+
     return (
         <div className={styles.header}>
             <ul className={styles.nav}>
                 <li className={styles.navItem}>
-                    <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+                    <NavLink
+                        className={calculateClassForNavLink}
+                        to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
                 </li>
                 <li className={styles.navItem}>
-                    <NavLink to={PATH.JUNIOR}>junior</NavLink>
+                    <NavLink
+                        className={calculateClassForNavLink}
+                        to={PATH.JUNIOR}>junior</NavLink>
                 </li>
                 <li className={styles.navItem}>
-                    <NavLink to={PATH.JUNIOR_PLUS}>junior+</NavLink>
+                    <NavLink
+                        className={calculateClassForNavLink}
+                        to={PATH.JUNIOR_PLUS}>junior+</NavLink>
                 </li>
             </ul>
 
